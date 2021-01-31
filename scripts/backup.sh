0c5ce9f5891aab36bd89308ed91d7a1e75cdacd7
k0r1 <minhquan.k0r1@gmail.com>
2021-01-31 16:32:23 +0700

    Update script

---
+++ scripts/backup.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+DATE=$(date +%H-%M-%S)
+BACKUP=db-$DATE.sql
+
+DB_HOST=$1
+DB_PASSWORD=$2
+DB_NAME=$3
+AWS_KEY_ID=$4
+AWS_SECRET=$5
+BUCKET_NAME=$6
+
+mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME > /tmp/$BACKUP && \
+export AWS_ACCESS_KEY_ID=$AWS_KEY_ID && \
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
+echo "Uploading your $BACKUP backup" && \
+aws s3 cp /tmp/db-$DATE.sql s3://$BUCKET_NAME/$BACKUP
